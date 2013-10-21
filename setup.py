a00c8d85310789564a7060097a584bac40cd46ce
Friedrich Lindenberg <friedrich@pudo.org>
2013-10-21 17:38:31 +0100

    python project skeleton

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opencontracts',
+    version='0.1',
+    description="Contract viewing news application",
+    long_description='',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    keywords='sql data procurement',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://pudo.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+    ],
+    tests_require=[],
+    entry_points=\
+    """ """,
+)
